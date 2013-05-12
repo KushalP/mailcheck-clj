fb672a812c88d9649ce437c62a24ee02eac627a2
Kushal Pisavadia <kushi.p@gmail.com>
2013-05-12 16:37:51 +0100

    Provide threshold, domains, and TLDs as single entities
    These aren't going to change in our app so provide them as (defonce)
    values.

--- src/mailcheck/core.clj
+++ src/mailcheck/core.clj
@@ -1,6 +1,12 @@
 (ns mailcheck.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce *threshold* 3)
+
+(defonce *domains*
+  ["aol.com" "att.net" "comcast.net" "facebook.com" "gmail.com"
+   "gmx.com" "google.com" "googlemail.com" "hotmail.co.uk"
+   "hotmail.com" "live.com" "mac.com" "mail.com" "me.com" "msn.com"
+   "sbcglobal.net" "verizon.net" "yahoo.co.uk" "yahoo.com"])
+
+(defonce *top-level-domains*
+  ["co.uk" "com" "edu" "gov" "info" "mil" "net" "org"])
