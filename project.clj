e347e5d7ded6359f4e87bfceaa9c83e3c2ffa839
Kushal Pisavadia <kushi.p@gmail.com>
2013-05-28 17:56:45 +0100

    Provide a useful description
    It's worth explaining the prior art here.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject mailcheck "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A port of Kicksend's mailcheck.js library to Clojure"
   :url "https://github.com/KushalP/mailcheck-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
