0531ffb5e17de463f7b4abce02400f414df94ff7
Kushal Pisavadia <kushi.p@gmail.com>
2013-05-28 17:56:04 +0100

    Set the URL to the Github repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mailcheck "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/KushalP/mailcheck-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
